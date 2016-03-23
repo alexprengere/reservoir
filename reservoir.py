23cbaa045524e80c77798761437809d6de14108a
Alex <alexprengere@gmail.com>
2016-03-23 10:19:59 -0400

    Better output

--- reservoir.py
+++ reservoir.py
@@ -3,14 +3,14 @@
 
 from __future__ import with_statement, print_function, division
 
-import sys
+from sys import stderr, stdin
 from random import random
 
 
 def build_reservoir(iterator, reservoir_size, verbose=False):
     if verbose:
         def log(s, *args):
-            print(s.format(*args), file=sys.stderr)
+            print(s.format(*args), file=stderr)
     else:
         def log(*_):
             pass
@@ -19,16 +19,16 @@ def build_reservoir(iterator, reservoir_size, verbose=False):
     try:
         for n, item in enumerate(iterator, start=1):
             if len(res) < reservoir_size:
-                log('> Adding at index {0}: {1!r}', len(res), item)
+                log('> [p=1] Adding at index {0}: {1!r}', len(res), item)
                 res.append(item)
             else:
                 ind = int(random() * n)
                 if ind < reservoir_size:
-                    log('> Swap at index {0}: {1!r} -> {2!r} (proba was {3}/{4})',
-                        ind, res[ind], item, reservoir_size, n)
+                    log('> [p={0}/{1}] Swap at index {2}: {3!r} -> {4!r}',
+                        reservoir_size, n, ind, res[ind], item)
                     res[ind] = item
     except KeyboardInterrupt:
-        log('! User interrupted the process, stopping now')
+        print('\n! User interrupted the process, stopping now\n', file=stderr)
 
     return res
 
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
-    for row in build_reservoir(sys.stdin,
+    for row in build_reservoir(stdin,
                                reservoir_size=args.size,
                                verbose=args.verbose):
         print(row, end="")
