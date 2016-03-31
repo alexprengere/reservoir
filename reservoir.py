5a832cbb0e0fec6607c76acc1fc4ddf91fc4a103
Alex <alexprengere@gmail.com>
2016-03-31 12:45:26 -0400

    Refactor, add option

--- reservoir.py
+++ reservoir.py
@@ -14,18 +14,19 @@ except NameError:
 
 from sys import stderr, stdin
 from random import random
-from math import log as ln
+from math import log
 
 
-def build_reservoir(data, R, verbose=False):
+def build_reservoir(data, R, threshold=None, verbose=False):
     if verbose:
-        def log(s, *args):
+        def p(s, *args):
             print(s.format(*args), file=stderr)
     else:
-        def log(*_):
+        def p(*_):
             pass
 
-    threshold = 4 * R
+    if threshold is None:
+        threshold = 4 * R
     gap = 0
     res = []
     try:
@@ -34,21 +35,21 @@ def build_reservoir(data, R, verbose=False):
             j += 1
             item = next(iterator)
             if len(res) < R:
-                log('> Adding at index {0}: {1!r}', len(res), item)
+                p('> Adding at index {0}: {1!r}', len(res), item)
                 res.append(item)
 
             elif j < threshold:
                 k = int(random() * j)
                 if k < R:
-                    log('> [p={0}/{1}] Swap at index {2}: {3!r} replaces {4!r}', R, j, k, item, res[k])
+                    p('> [p={0}/{1}] Swap at index {2}: {3!r} replaces {4!r}', R, j, k, item, res[k])
                     res[k] = item
             else:
-                gap = int(ln(random()) / ln(1 - R / j))
+                gap = int(log(random()) / log(1 - R / j))
+                j += gap
                 for _ in range(gap):
                     item = next(iterator)
-                j += gap
                 k = int(random() * R)
-                log('> Swap after gap {0} at index {1}: {2!r} replaces {3!r}', gap, k, item, res[k])
+                p('> After gap {0}, swap at index {1}: {2!r} replaces {3!r}', gap, k, item, res[k])
                 res[k] = item
 
     except KeyboardInterrupt:
@@ -64,10 +65,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('size', help="Reservoir size", type=int)
+    parser.add_argument('-t', '--threshold', help="Threshold for using gaps", type=int)
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
     for row in build_reservoir(stdin,
                                R=args.size,
+                               threshold=args.threshold,
                                verbose=args.verbose):
         print(row, end="")
